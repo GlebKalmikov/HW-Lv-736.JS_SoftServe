beed359ee0f702411dd24f2c1ecd63e26b79edc1
dvorko <uzinggaming@gmail.com>
2022-03-15 18:59:20 +0000

    olek block changed

--- js/navmenu.js
+++ js/navmenu.js
@@ -1,21 +1,6 @@
-(function () {
-    const iconBurger = document.querySelector('.navigation-burger');
-    const navigationsBody = document.querySelector('.navigation-wrapper');
-    iconBurger.addEventListener('click', function (e) {
-      document.body.classList.toggle('lock');
-      iconBurger.classList.toggle('active');
-      navigationsBody.classList.toggle('active');
-    });
-  
-    const navLinks = document.querySelectorAll('.navigation__link');
-    navLinks.forEach((navLink) => {
-      navLink.addEventListener('click', (_) => {
-        if (iconBurger.classList.contains('active')) {
-          document.body.classList.remove('lock');
-          iconBurger.classList.remove('active');
-          navigationsBody.classList.remove('active');
-        }
-      });
-    });
-  })();
-  
+$(document).ready(function() {
+	$('.header_burger').click(function(event) {
+		$('.header_burger,.header_menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
