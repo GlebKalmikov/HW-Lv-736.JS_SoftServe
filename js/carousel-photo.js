fc51e7fd3477b647826c9f02b1e76beee2de4f4e
Lena7L <lena.litvinchuk1@gmail.com>
2022-03-25 07:00:59 +0000

    Created js

---
+++ js/carousel-photo.js
@@ -0,0 +1,31 @@
+(function() {
+
+const photo = [
+    {
+        id: 1,
+        image: 'img/stoper.png'
+    },
+{
+    id: 2,
+    image: 'img/stoper.png'
+},
+{
+    id: 3,
+    image: 'img/stoper.png'
+},
+{
+    id: 4,
+    image: 'img/stoper.png'
+},
+{
+    id: 5,
+    image: 'img/stoper.png'
+},
+{
+    id: 6,
+    image: 'img/stoper.png'
+}
+];
+
+
+}) ();
