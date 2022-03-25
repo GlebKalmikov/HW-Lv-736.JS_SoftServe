8df7e666566511fabb8b9f3a884cb9cefcf00e4a
Val-Osad <v.p.osadchuk@gmail.com>
2022-03-25 14:55:48 +0000

    pathetic attempts of javascript

--- js/age-checker.js
+++ js/age-checker.js
@@ -1,3 +1,12 @@
-(function() {
+(ageChecker() {
+    const confirmWindow = document.querySelector(.age-checker);
+    const overageButton = document.querySelector(.overage-button);
+    const underageButton = document.querySelector(.underage-button);
+
+    overageButton.addEventListener('click', confirmWindow.innerHTML = '');
+    
+    function goToNotAllowedPage(){
+    window.location="not-allowed.html";
+}
 
 }) ();
