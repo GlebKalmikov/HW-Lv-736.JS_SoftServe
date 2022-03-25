df6bd6e7415450b0983f90965796040d6c7991f4
Val-Osad <100376201+Val-Osad@users.noreply.github.com>
2022-03-25 17:22:06 +0200

    Update age-checker.js

--- js/age-checker.js
+++ js/age-checker.js
@@ -1,9 +1,10 @@
 (ageChecker() {
-    const confirmWindow = document.querySelector(.age-checker);
-    const overageButton = document.querySelector(.overage-button);
-    const underageButton = document.querySelector(.underage-button);
+    const confirmWindow = document.querySelector(".age-checker");
+    const overageButton = document.querySelector(".overage-button");
+    const underageButton = document.querySelector(".underage-button");
 
     overageButton.addEventListener('click', confirmWindow.innerHTML = '');
+    underageButton.addEventListener('click', goToNotAllowedPage());
     
     function goToNotAllowedPage(){
     window.location="not-allowed.html";
