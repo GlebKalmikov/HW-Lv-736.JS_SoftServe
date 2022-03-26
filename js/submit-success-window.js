7239d68e0d5454c6e996d570607d59404d52237c
Val-Osad <v.p.osadchuk@gmail.com>
2022-03-26 14:24:07 +0000

    formatting plus js for submit window

---
+++ js/submit-success-window.js
@@ -0,0 +1,27 @@
+(function () {
+    let submitSuccessWindow = document.querySelector(".submit-success-alert");
+    const submitButton = document.querySelector(".join-submit");
+  
+    submitButton.addEventListener("click", () => submitSuccessWindow.innerHTML += 
+        `<div class="submit-success-alert">
+            <div class="submit-success-alert-window">
+                <div class="cross-exit"></div>
+                <img src="img/checkmark.jpg" alt="">
+                <h5>Thank you!</h5>
+                <h6>We will contact you shortly.</h6>
+                <a href="#" class="orange-button-ok">Ok</a>
+            </div>
+        </div>`
+     );
+
+    
+    const crossButton = document.querySelector(".cross-exit");
+    const okButton = document.querySelector(".orange-button-ok");
+ 
+    crossButton.addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
+    okButton.addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
+    
+  
+    
+  })();
+  
