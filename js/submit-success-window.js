6e0a53cd0af3eb818170580adee918f2c06a91db
Val-Osad <v.p.osadchuk@gmail.com>
2022-03-28 16:43:18 +0000

    js submit window fixed

--- js/submit-success-window.js
+++ js/submit-success-window.js
@@ -2,25 +2,17 @@
     let submitSuccessWindow = document.querySelector(".submit-success-alert");
     const submitButton = document.querySelector(".join-submit");
   
-    submitButton.addEventListener("click", () => submitSuccessWindow.innerHTML += 
+    submitButton.addEventListener("click", () => {
+        submitSuccessWindow.innerHTML += 
         `<div class="submit-success-alert-window">
             <div class="cross-exit"></div>
             <img src="img/checkmark.jpg" alt="">
             <h5>Thank you!</h5>
             <h6>We will contact you shortly.</h6>
             <a class="orange-button-ok">Ok</a>
-        </div>`
-        const crossButton = document.querySelector(".cross-exit");
-        crossButton.addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
+        </div>`;
+        document.querySelector(".cross-exit").addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
+        document.querySelector(".orange-button-ok").addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
+        }   
     );
-    
-    
-    
-    
-    // submitSuccessWindow.addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
-    // const crossButton = document.querySelector(".cross-exit");
-    // const okButton = document.querySelector(".orange-button-ok");
-     
-    // crossButton.addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
-    // okButton.addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
   })();
