3b79db22c676e0d56d9f48ee1bcf2f119af8b7c6
Val-Osad <100376201+Val-Osad@users.noreply.github.com>
2022-03-31 17:53:08 +0300

    new dark background added

--- js/submit-success-window.js
+++ js/submit-success-window.js
@@ -4,12 +4,14 @@
   
     submitButton.addEventListener("click", () => {
         submitSuccessWindow.innerHTML += 
-        `<div class="submit-success-alert-window">
-            <div class="cross-exit"></div>
-            <img src="img/checkmark.jpg" alt="">
-            <h5>Thank you!</h5>
-            <h6>We will contact you shortly.</h6>
-            <a class="orange-button-ok">Ok</a>
+        `<div class="submit-success-alert-window-background">
+            <div class="submit-success-alert-window">
+                <div class="cross-exit"></div>
+                <img src="img/checkmark.jpg" alt="">
+                <h5>Thank you!</h5>
+                <h6>We will contact you shortly.</h6>
+                <a class="orange-button-ok">Ok</a>
+            </div>
         </div>`;
         document.querySelector(".cross-exit").addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
         document.querySelector(".orange-button-ok").addEventListener("click", () => submitSuccessWindow.innerHTML = "" );
